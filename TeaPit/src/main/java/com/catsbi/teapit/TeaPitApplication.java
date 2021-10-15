ba04d47ace22a7a8c5b7ec706ef529924338740c
catsbi <killfine1988@gmail.com>
2021-10-15 19:40:25 +0900

    feat: first commit Spring Boot

---
+++ TeaPit/src/main/java/com/catsbi/teapit/TeaPitApplication.java
@@ -0,0 +1,13 @@
+package com.catsbi.teapit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaPitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaPitApplication.class, args);
+    }
+
+}
